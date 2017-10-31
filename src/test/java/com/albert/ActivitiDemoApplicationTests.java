1a71348faa6c08d37ee6015c03ed9e95afe257e7
AlbertZhang <790321193@qq.com>
2017-10-31 08:01:20 +0800

    ---

--- src/test/java/com/albert/ActivitiDemoApplicationTests.java
+++ src/test/java/com/albert/ActivitiDemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.albert;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,8 +8,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ActivitiDemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
 }
